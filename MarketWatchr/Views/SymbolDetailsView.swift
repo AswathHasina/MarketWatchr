c240e33a2077760e7222c11c71d8fa5d03c91aee
AswathHasina <syedhasina09@gmail.com>
2025-11-25 20:00:15 +0400

    Creating views

---
+++ MarketWatchr/Views/SymbolDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  SymbolDetailsView.swift
+//  MarketWatchr
+//
+//  Created by Aswath on 25/11/25.
+//
+
+import SwiftUI
+
+struct SymbolDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SymbolDetailsView()
+}
