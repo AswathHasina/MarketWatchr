1965462ac5d2cd14e4531f7b9feea20602b6b009
AswathHasina <syedhasina09@gmail.com>
2025-12-07 16:34:12 +0400

    Initial commit

---
+++ MarketWatchr/MarketWatchrApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketWatchrApp.swift
+//  MarketWatchr
+//
+//  Created by Aswath on 25/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MarketWatchrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
